4c0b5bfab716da926b4ccd80f1c62b66c62a6737
chenxi712@gmail.com <chenxi712@gmail.com>
2015-09-01 17:50:46 +0800

    update

---
+++ Assets/Scripts/Scenes/TouchCheck.cs
@@ -0,0 +1,85 @@
+﻿using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+
+public class TouchCheck : MonoBehaviour
+{
+	public GameObject cross;
+	public GameObject button;
+
+	private int remainTimes;
+	private int screenWidth;
+	private int screenHeight;
+	// LCD Device positions
+	private List<Vector2> lcdPos = new List<Vector2>();
+	// Touch-screen positions
+	private List<Vector2> tsPos = new List<Vector2>();
+
+	void Start()
+	{
+		screenWidth = Screen.width;
+		screenHeight = Screen.height;
+		remainTimes = 5;
+		button.SetActive(false);
+		cross.SetActive(true);
+	}
+	
+	void Update()
+	{
+		HandleInput();
+	}
+
+	private void LEqations3x3(double[,] det)
+	{
+		double x, y, z;
+		Utils.LEqations3x3(det, out x, out y, out z);
+	}
+
+	private void HandleInput()
+	{
+		if (Application.platform == RuntimePlatform.WindowsEditor || Application.platform == RuntimePlatform.OSXEditor)
+		{
+			if (remainTimes > 0)
+			{
+				if (Input.GetMouseButtonUp(0))
+				{
+					Vector2 touchPos = Input.mousePosition;
+					lcdPos.Add(touchPos);
+					tsPos.Add(touchPos);
+					--remainTimes;
+					TranslateCross();
+				}
+			}
+			else
+			{
+				Vector2 touchPos = Input.mousePosition;
+//				if (Vector2.)
+			}
+		}
+	}
+
+	private void TranslateCross()
+	{
+		if (remainTimes == 4)
+		{
+			iTween.MoveTo(cross, iTween.Hash("time", 0.6, "islocal", true, "x", -800, "y", 400));
+		}
+		else if (remainTimes == 3)
+		{
+			iTween.MoveTo(cross, iTween.Hash("time", 0.6, "islocal", true, "x", -800, "y", -400));
+		}
+		else if (remainTimes == 2)
+		{
+			iTween.MoveTo(cross, iTween.Hash("time", 0.6, "islocal", true, "x", 800, "y", -400));
+		}
+		else if (remainTimes == 1)
+		{
+			iTween.MoveTo(cross, iTween.Hash("time", 0.6, "islocal", true, "x", 800, "y", 400));
+		}
+		else if (remainTimes == 0)
+		{
+			cross.SetActive(false);
+			button.SetActive(true);
+		}
+	}
+}
--- Assets/Scripts/Utils/Utils.cs
+++ Assets/Scripts/Utils/Utils.cs
@@ -34,4 +34,45 @@ public static class Utils
 		return ipv4;
 	}
 
+	public static void LEqations3x3(double[,] det, out double x, out double y, out double z)
+	{
+		x = y = z = 0;
+		double[,] det0 = new double[3, 3];
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++) det0[i, j] = det[i, j];
+		double det00 = determinant(det0);
+		double[,] detx = new double[3, 3];
+		detx[0, 0] = det[0, 3];
+		detx[1, 0] = det[1, 3];
+		detx[2, 0] = det[2, 3];
+		for (int i = 0; i < 3; i++)
+			for (int j = 1; j < 3; j++) detx[i, j] = det[i, j];
+		double detx0 = determinant(detx);
+		double[,] dety = new double[3, 3];
+		dety[0, 1] = det[0, 3];
+		dety[1, 1] = det[1, 3];
+		dety[2, 1] = det[2, 3];
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 3; j++)
+		{
+			if (j != 1)
+				dety[i, j] = det[i, j];
+		}
+		double dety0 = determinant(dety);
+		double[,] detz = new double[3, 3];
+		detz[0, 2] = det[0, 3];
+		detz[1, 2] = det[1, 3];
+		detz[2, 2] = det[2, 3];
+		for (int i = 0; i < 3; i++)
+			for (int j = 0; j < 2; j++) detz[i, j] = det[i, j];
+		double detz0 = determinant(detz);
+		x = detx0 / det00;
+		y = dety0 / det00;
+		z = detz0 / det00;
+	}
+
+	private static double determinant(double[,] x)//3×3行列式
+	{
+		return x[0, 0] * x[1, 1] * x[2, 2] + x[1, 0] * x[2, 1] * x[0, 2] + x[0, 1] * x[1, 2] * x[2, 0] - x[2, 0] * x[1, 1] * x[0, 2] - x[2, 1] * x[1, 2] * x[0, 0] - x[1, 0] * x[0, 1] * x[2, 2];
+	}
 }
