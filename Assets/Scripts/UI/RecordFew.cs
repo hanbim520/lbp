cada6d4fe4b259ea05ff55f04b9ad175a6bd857b
chenxi <chenxi712@gmail.com>
2015-09-08 03:02:21 +0800

    update

--- Assets/Scripts/UI/RecordFew.cs
+++ Assets/Scripts/UI/RecordFew.cs
@@ -1,14 +1,18 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
 using System.Collections.Generic;
 
 public class RecordFew : MonoBehaviour
 {
-	private List<int> few = new List<int>();
-	private int fewValue = 0;
+    public GameObject[] fewGO;
+    public Sprite[] images;
+	private List<int> fewValues = new List<int>();
+    private int fixedCount;
 
 	void Start()
 	{
+        fixedCount = fewGO.Length;
 		HandleRefreshRecord();
 		GameEventManager.RefreshRecord += HandleRefreshRecord;
 	}
@@ -20,7 +24,16 @@ public class RecordFew : MonoBehaviour
 
 	private void HandleRefreshRecord(int result = -1)
 	{
-		Dictionary<int, int> dict = new Dictionary<int, int>();
+        if (GameData.GetInstance().records.Count == 0)
+        {
+            foreach (GameObject item in fewGO)
+            {
+                item.SetActive(false);
+            }
+            return;
+        }
+
+        Dictionary<int, int> dict = new Dictionary<int, int>();
 		for (int i = 0; i < 37; ++i)
 		{
 			dict.Add(i, 0);
@@ -29,17 +42,30 @@ public class RecordFew : MonoBehaviour
 		{
 			dict[item] += 1;
 		}
-		foreach (var item in dict)
-		{
-			if (item.Value <= fewValue)
-			{
-				few.Add(item.Key);
-			}
-		}
+        List<KeyValuePair<int, int>> lst = new List<KeyValuePair<int, int>>(dict);
+        lst.Sort(delegate(KeyValuePair<int, int> s1, KeyValuePair<int, int> s2) {return s1.Value.CompareTo(s2.Value);});
+
+        fewValues.Clear();
+        for (int i = 0; i < fixedCount; ++i)
+        {
+            fewValues.Add(lst[i].Key);
+        }
+        RefreshFew();
 	}
 
-	private void RefreshFew()
+	public void RefreshFew()
 	{
+        for (int i = 0; i < fewValues.Count; ++i)
+        {
+            if (i > fixedCount)
+                break;
 
+            fewGO[i].SetActive(true);
+            if (GameData.GetInstance().colorTable[fewValues[i]] == ResultType.Red)
+                fewGO[i].transform.FindChild("Image").GetComponent<Image>().sprite = images[0];
+            else
+                fewGO[i].transform.FindChild("Image").GetComponent<Image>().sprite = images[1];
+            fewGO[i].transform.FindChild("Text").GetComponent<Text>().text = fewValues[i].ToString();
+        }
 	}
 }
--- Assets/Scripts/UI/RecordMost.cs
+++ Assets/Scripts/UI/RecordMost.cs
@@ -1,16 +1,71 @@
 ﻿using UnityEngine;
+using UnityEngine.UI;
 using System.Collections;
+using System.Collections.Generic;
 
 public class RecordMost : MonoBehaviour
 {
-
-	void Start()
-	{
-	
-	}
-	
-	void Update()
-	{
-	
-	}
+    public GameObject[] fewGO;
+    public Sprite[] images;
+    private List<int> fewValues = new List<int>();
+    private int fixedCount;
+    
+    void Start()
+    {
+        fixedCount = fewGO.Length;
+        HandleRefreshRecord();
+        GameEventManager.RefreshRecord += HandleRefreshRecord;
+    }
+    
+    void OnDestroy()
+    {
+        GameEventManager.RefreshRecord -= HandleRefreshRecord;
+    }
+    
+    private void HandleRefreshRecord(int result = -1)
+    {
+        if (GameData.GetInstance().records.Count == 0)
+        {
+            foreach (GameObject item in fewGO)
+            {
+                item.SetActive(false);
+            }
+            return;
+        }
+        
+        Dictionary<int, int> dict = new Dictionary<int, int>();
+        for (int i = 0; i < 37; ++i)
+        {
+            dict.Add(i, 0);
+        }
+        foreach (int item in GameData.GetInstance().records)
+        {
+            dict[item] += 1;
+        }
+        List<KeyValuePair<int, int>> lst = new List<KeyValuePair<int, int>>(dict);
+        lst.Sort(delegate(KeyValuePair<int, int> s1, KeyValuePair<int, int> s2) {return s2.Value.CompareTo(s1.Value);});
+        
+        fewValues.Clear();
+        for (int i = 0; i < fixedCount; ++i)
+        {
+            fewValues.Add(lst[i].Key);
+        }
+        RefreshFew();
+    }
+    
+    public void RefreshFew()
+    {
+        for (int i = 0; i < fewValues.Count; ++i)
+        {
+            if (i > fixedCount)
+                break;
+            
+            fewGO[i].SetActive(true);
+            if (GameData.GetInstance().colorTable[fewValues[i]] == ResultType.Red)
+                fewGO[i].transform.FindChild("Image").GetComponent<Image>().sprite = images[0];
+            else
+                fewGO[i].transform.FindChild("Image").GetComponent<Image>().sprite = images[1];
+            fewGO[i].transform.FindChild("Text").GetComponent<Text>().text = fewValues[i].ToString();
+        }
+    }
 }
