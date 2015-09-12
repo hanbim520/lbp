959c6a59ccd40ed292b785ff0358506427a44d4f
chenxi <chenxi712@gmail.com>
2015-09-12 21:16:22 +0800

    update

---
+++ Assets/Scripts/Utils/FrameAnim.cs
@@ -0,0 +1,35 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+using System.Collections;
+
+public class FrameAnim : MonoBehaviour
+{
+	public float fps = 10.0f;
+	public Sprite[] ani;
+
+	private Image image;
+	private float timeElasped = 0;
+	private int curFrame = 0;
+	
+	void Start ()
+	{
+		image = GetComponent<Image> ();
+	}
+	
+	void FixedUpdate ()
+	{
+		timeElasped += Time.fixedDeltaTime;
+		
+		if(timeElasped >= 1.0 / fps)
+		{
+			timeElasped = 0;
+			++curFrame;
+
+			if(curFrame >= ani.Length)
+			{
+				curFrame = 0;
+			}
+			image.overrideSprite = ani[curFrame];
+		}
+	}
+}
