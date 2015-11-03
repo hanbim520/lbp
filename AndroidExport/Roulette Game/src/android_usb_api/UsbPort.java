3a54679c9acab3d9353313819137b35076715353
chenxi712@gmail.com <chenxi712@gmail.com>
2015-11-03 18:14:19 +0800

    update

--- AndroidExport/Roulette Game/src/android_usb_api/UsbPort.java
+++ AndroidExport/Roulette Game/src/android_usb_api/UsbPort.java
@@ -1,40 +1,20 @@
 package android_usb_api;
 
-import java.io.FileDescriptor;
-
-import android.hardware.usb.*;
-import android.os.ParcelFileDescriptor;
-
 public class UsbPort {
-//	private final UsbDevice mDevice;
-    // used by the JNI code
-    private int mNativeContext;
+    private int mDeviceHandle;
+    public final int vid = 0x0483;
+    public final int pid = 0x5750;
+	 
+    public native int open(int vid, int pid);
+    public native void close(int deviceHandle);
+    public native int write(int deviceHandle, byte[] data);
+    public native byte[] read(int deviceHandle);
     
-    public native boolean open(String name, FileDescriptor descriptor);
-    public native void close();
-    public native boolean usbClaimInterface(int interfaceID, boolean force);
-    public native int usbControlRequest(int requestType, int request, int value,
-            int index, byte[] buffer, int length, int timeout);
-    public native int usbBulkRequest(int endpointAddress, byte[] buffer, int length, int timeout);
- 
-    public boolean open(String name, ParcelFileDescriptor pfd) {
-        return open(name, pfd.getFileDescriptor());
-    }
-
-    public boolean claimInterface(UsbInterface intf, boolean force) {
-        return usbClaimInterface(intf.getId(), force);
-    }
-     
-    public int controlTransfer(int requestType, int request, int value,
-            int index, byte[] buffer, int length, int timeout) {
-        return usbControlRequest(requestType, request, value, index, buffer, length, timeout);
-    }
- 
-    public int bulkTransfer(UsbEndpoint endpoint, byte[] buffer, int length, int timeout) {
-        return usbBulkRequest(endpoint.getAddress(), buffer, length, timeout);
+    public int getDeviceHandle() {
+    	return mDeviceHandle;
     }
     
     static {
-		System.loadLibrary("usbhost");
+		System.loadLibrary("usb1.0");
 	}
 }
