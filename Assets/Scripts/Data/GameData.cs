38e47bfc03c961813a947b7d586718401cb4f604
chenxi712@gmail.com <chenxi712@gmail.com>
2016-10-08 18:18:18 +0800

    update

--- Assets/Scripts/Data/GameData.cs
+++ Assets/Scripts/Data/GameData.cs
@@ -9,7 +9,7 @@ using System.Collections.Generic;
  */
 public class GameData
 {
-	public static bool debug 		= true;		// 是否模拟出球
+	public static bool debug 		= false;		// 是否模拟出球
 	public static bool controlCode	= false;		// 是否打码
 
     // Setting menu
--- Assets/Scripts/Scenes/StartInfo.cs
+++ Assets/Scripts/Scenes/StartInfo.cs
@@ -7,10 +7,10 @@ public class StartInfo : MonoBehaviour
 	{
 		GameData.GetInstance().ReadDataFromDisk();
 		LoadUpdateUtils();
-		Screen.SetResolution(GameData.GetInstance().resolutionWidth, GameData.GetInstance().resolutionHeight, true);
 		if (GameData.GetInstance().deviceIndex > 0 && 
 		    GameData.GetInstance().deviceIndex < GameData.GetInstance().monitorDeviceIndex)
 		{
+			Screen.SetResolution(GameData.GetInstance().resolutionWidth, GameData.GetInstance().resolutionHeight, true);
 //			LoadInputDevice();
 			LoadHIDUtils();
 			LoadNetwork();
@@ -26,6 +26,7 @@ public class StartInfo : MonoBehaviour
 		}
 		else
 		{
+			Screen.SetResolution(GameData.GetInstance().resolutionWidth, GameData.GetInstance().resolutionHeight, true);
 			Application.LoadLevel(Scenes.Backend);
 		}
 	}
