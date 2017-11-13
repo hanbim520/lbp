03f8005d77ef60a9a0fef30ab7eb463872f7fb16
lfwelove <chenxi712@gmail.com>
2017-11-13 19:12:40 +0800

    add app version display

---
+++ Assets/Scripts/UI/UIVersion.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class UIVersion : MonoBehaviour 
+{
+	public Text txtversion;
+
+	void Start () 
+	{
+		txtversion.text = string.Format("Version: {0}", GameData.version);
+	}
+	
+}
